ca09e2a8d3f4ae9dc52c32eebcf0f558de938e2a
HokieGeek <andres.f.perez@gmail.com>
2019-07-08 21:22:40 -0400

    First functional commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # godemo
+
+Silly little time-saver that takes [github.com/hokiegeek/gonexus](//github.com/hokiegeek/gonexus) and provides some shortcuts to it with an emphasis on my demo instances on localhost with standard ports and logins.
+
+An example program:
+```go
+package main
+
+import (
+	"fmt"
+
+	"demo"
+)
+
+func main() {
+    // Print all repositories in RM on localhost:8081
+	repos, _ := demo.Repos()
+	fmt.Printf("%v\n", repos)
+
+    // Print all applications in IQ on localhost:8070
+	apps, _ := demo.Apps()
+	fmt.Printf("%v\n", apps)
+
+	// Create a new organization in IQ on localhost:8070
+	iq, _ := demo.IQ()
+	iq.CreateOrganization("foobar")
+}
+```
---
+++ demo.go
@@ -0,0 +1,29 @@
+package demo
+
+import (
+	privateiq "github.com/hokiegeek/gonexus-private/iq"
+	"github.com/hokiegeek/gonexus/iq"
+	"github.com/hokiegeek/gonexus/rm"
+)
+
+func RM() (*nexusrm.RM, error) {
+	return nexusrm.New("http://localhost:8081", "admin", "admin123")
+}
+
+func IQ() (*nexusiq.IQ, error) {
+	return nexusiq.New("http://localhost:8070", "admin", "admin123")
+}
+
+func IQp() (*privateiq.IQ, error) {
+	return privateiq.New("http://localhost:8070", "admin", "admin123")
+}
+
+func Repos() ([]nexusrm.Repository, error) {
+	rm, _ := RM()
+	return nexusrm.GetRepositories(rm)
+}
+
+func Apps() ([]nexusiq.ApplicationDetails, error) {
+	iq, _ := IQ()
+	return nexusiq.GetAllApplications(iq)
+}
