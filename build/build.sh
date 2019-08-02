be83b193a71ba9a9aef2e1d46f31d8ad652746af
HokieGeek <andres.f.perez@gmail.com>
2019-08-02 16:25:29 -0400

    Added Q&D build script

---
+++ build/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+GOOS=linux GOARCH=amd64 go build -o nx-nix .
+GOOS=darwin GOARCH=amd64 go build -o nx-mac .
+GOOS=windows GOARCH=amd64 go build -o nx-win.exe .
+tar -cf nexus-cli.tar nx-nix nx-mac nx-win.exe
+rm nx-nix nx-mac nx-win.exe
